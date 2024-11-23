92fda63b66104d6ac9b3a89078e6d80a6480a970
jjjung1 <jwuv00@gmail.com>
2024-11-23 18:27:37 +0900

    드롭 케이스 코드

--- Today's Menu/Assets/Codes/DropZone.cs
+++ Today's Menu/Assets/Codes/DropZone.cs
@@ -1,17 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DropZone : MonoBehaviour
 {
+    public string dropZoneName;
+    public Image foodImage; //resultCanvas의 그 데이터
+    public Text foodNameText; //resultCanvas의 그 데이터
+
+    //요리 종류 쭉..
+    public Sprite OmuriceImg;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Draggable"))
         {
             SetActive externalCode = FindObjectOfType<SetActive>();
+
             if (externalCode != null)
             {
-                externalCode.OnOffCanvas();
+                string objectName = other.gameObject.name;
+                bool handled = false;
+
+                if (dropZoneName == "rice")
+                {
+                    handled = HandleDropZoneRice(objectName);
+                }
+                else if (dropZoneName == "bread")
+                {
+                    handled = HandleDropZoneBread(objectName);
+                }
+                else if (dropZoneName == "noodle")
+                {
+                    handled = HandleDropZoneNoodle(objectName);
+                }
+
+                if (handled)
+                {
+                    externalCode.OnOffCanvas();
+                }
             }
             else
             {
@@ -19,4 +47,61 @@ public class DropZone : MonoBehaviour
             }
         }
     }
+
+    private bool HandleDropZoneRice(string objectName)
+    {
+        switch (objectName)
+        {
+            case "meat":
+                SetFoodInfo(OmuriceImg, "오므라이스"); 
+                break;
+            default:
+                break;
+        }
+
+        return true;
+    }
+
+    private bool HandleDropZoneBread(string objectName)
+    {
+        switch (objectName)
+        {
+            case "meat":
+                SetFoodInfo(OmuriceImg, "오므라이스");
+                break;
+            default:
+                break;
+        }
+
+        return true;
+    }
+
+    private bool HandleDropZoneNoodle(string objectName)
+    {
+        switch (objectName)
+        {
+            case "meat":
+                SetFoodInfo(OmuriceImg, "오므라이스"); 
+                break;
+            default:
+                break;
+        }
+
+        return true;
+    }
+
+    private void SetFoodInfo(Sprite newSprite, string newText)
+    {
+        if (foodImage != null)
+        {
+            foodImage.sprite = newSprite;
+            foodNameText.text = newText;
+        }
+        else
+        {
+            Debug.LogError("FoodImage가 설정되지 않았습니다.");
+        }
+    }
 }
+
+
